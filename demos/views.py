8db4823caa9efd67f220c112fbd393a2870ace0f
literalllyme1 <kde7891@naver.com>
2023-06-30 18:49:15 +0900

    first_calculator

---
+++ demos/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def calculator(request):
+    # return HttpResponse('계산기 기능 구현 시작입니다.')
+
+    #데이터 확인 
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    operators = request.GET.get('operators')
+
+    #계산
+    
+    if operators == '+':
+        result = num1 + num2
+    
+    elif operators == '-':
+        if(num1>num2):
+            result = num1 - num2
+        else:
+            result = num2 - num1
+    elif operators == '*':
+        result = num1 * num2
+    elif operators == '/':
+        result = num1 / num2
+    else:
+        result = 0
+
+    #응답 
+    return render(request, 'calculator.html', {'result': result})
